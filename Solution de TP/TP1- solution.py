45cd7a3e6f93ebed9a8c44629b5e4bfdd360c51a
sihembouaricha248-ops <sihembouaricha248@gmail.com>
2025-12-11 20:35:12 +0100

    Add functionality to filter and analyze sequences

--- Solution de TP/TP1- solution.py
+++ Solution de TP/TP1- solution.py
@@ -11,3 +11,12 @@ df = pd.DataFrame(data)
 # 2) اختيار و عرض العمود 'Longueur' 
 print(df["Longueur"].
       
+print("Question 3 : sélectionner les séquences dont la longueur est > 10")
+seq_gt_10 = df[df["Longueur"] > 10]
+print("Séquences > 10 :")
+print(seq_gt_10)
+
+print("Question 4 : calculer la moyenne du %GC avec 3 chiffres après la virgule")
+moy_gc = df["Pourcentage_GC"].mean()
+moy_gc = round(moy_gc, 3)
+print("Moyenne GC :", moy_gc)
